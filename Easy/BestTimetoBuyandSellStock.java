13dc5696f7480cc2f5f5c0c5fad413b33ab4038a
Ankur yadav <165983779+Ankurscode@users.noreply.github.com>
2024-10-16 17:46:54 +0530

    Add files via upload

---
+++ Easy/BestTimetoBuyandSellStock.java
@@ -0,0 +1,20 @@
+public class BestTimetoBuyandSellStock {
+    public static int maxProfit(int[] prices) {
+        int buyPrice=prices[0];
+        int profit=0;
+        
+        for (int i = 1; i < prices.length; i++) {
+            if(buyPrice>prices[i]){
+                buyPrice= prices[i];
+            }
+            profit=Math.max(profit, prices[i]-buyPrice);
+        }
+       
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int [] prices = {7,1,5,3,6,4};
+       System.out.println(maxProfit(prices));
+    }
+}
